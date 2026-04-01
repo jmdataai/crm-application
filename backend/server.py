29b180e66da651116dd38afd6c1c8dbe5d25a0ce
Ravi <freddy@jmdataai.com>
2026-04-01 10:53:10 +0530

    Added * to allow_origins tocheck frontend requests

--- backend/server.py
+++ backend/server.py
@@ -1090,7 +1090,8 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get("CORS_ORIGINS", "http://localhost:3000").split(","),
+    # allow_origins=os.environ.get("CORS_ORIGINS", "http://localhost:3000").split(","),
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
